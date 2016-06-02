07ee423b26467b7f719898bf2bdca9f65cb21aaf
pomochieng <pomochieng@gmail.com>
2016-06-02 12:59:25 +0300

    sign up -A user Resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   root 'static_pages#home'
-
   get 'help' => 'static_pages#help'
-
   get 'about' => 'static_pages#about'
-
-  get 'cv' => 'static_pages/cv'
-
+  get 'cv' => 'static_pages#cv'
+  get 'signup' => 'users#new'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
