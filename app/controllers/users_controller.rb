e0f117d81fe8d101ade67789a6cbc519b6673bd1
pomochieng <pomochieng@gmail.com>
2016-06-07 16:55:56 +0300

    updating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,24 @@ class UsersController < ApplicationController
   def create
 			@user = User.new(user_params)				# Not the final implementation!
 			if @user.save
-				# Handle a successful save.
+				log_in @user
+				flash[:success] = "Welcome to the Sample App!"
+				redirect_to @user
 			else
-			render 'new'
+				render 'new'
+		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			# Handle a successful update.
+		else
+			render 'edit'
 		end
 	end
 
@@ -22,5 +37,4 @@ private
 		params.require(:user).permit(:name, :email, :password,
 									:password_confirmation)
 	end
-
 end
