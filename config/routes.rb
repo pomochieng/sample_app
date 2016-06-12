1511e7295403682b1c84d77bbb3860ae16446d2d
pomochieng <pomochieng@gmail.com>
2016-06-12 17:20:45 +0300

    suprised errors

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ get		'signup'		=> 	'users#new'
 get		'login'			=> 	'sessions#new'
 post	'login'			=> 	'sessions#create'
 delete 	'logout' 		=> 	'sessions#destroy'
+resources :microposts
 resources :users
 
 end
