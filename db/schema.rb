9e05c14b7acbfec3720fd71a59bd8ceecca89349
pomochieng <pomochieng@gmail.com>
2016-06-13 00:17:14 +0300

    Starting Microposts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160611095608) do
+ActiveRecord::Schema.define(version: 20160612142505) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
